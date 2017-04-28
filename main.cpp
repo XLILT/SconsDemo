b3b388536acbb860f411842e1b2331d20ca4eb15
XLILT <xiaolongma002@163.com>
2017-04-28 11:42:21 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: main.cpp
+    > Author: mxl2
+    > Mail: xiaolongicx@gmail.com
+    > Created Time: Fri 28 Apr 2017 10:05:03 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+#include "a.pb.h"
+
+int main()
+{
+    SearchRequest sr;
+    return 0;
+}
+
